accc2d034778da1bc2ca8dab3da89f6acda28b0c
Юлия Иванцова <cstud110912@vyatsu.ru>
2020-06-21 12:43:39 +0300

    commit client ui + code#1

---
+++ РосБизнесСофт/FormClients.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace РосБизнесСофт
+{
+    partial class FormClients
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.labelBusuness = new System.Windows.Forms.Label();
+            this.labelFIO = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxBusuness = new System.Windows.Forms.TextBox();
+            this.textBoxFIO = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.listViewClients = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonAdd.Location = new System.Drawing.Point(212, 307);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonEdit.Location = new System.Drawing.Point(334, 307);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "Изменить";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            this.buttonEdit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonDel.Location = new System.Drawing.Point(445, 307);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(75, 23);
+            this.buttonDel.TabIndex = 3;
+            this.buttonDel.Text = "Удалить";
+            this.buttonDel.UseVisualStyleBackColor = false;
+            // 
+            // labelBusuness
+            // 
+            this.labelBusuness.AutoSize = true;
+            this.labelBusuness.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.labelBusuness.Location = new System.Drawing.Point(29, 18);
+            this.labelBusuness.Name = "labelBusuness";
+            this.labelBusuness.Size = new System.Drawing.Size(73, 13);
+            this.labelBusuness.TabIndex = 4;
+            this.labelBusuness.Text = "Организация";
+            // 
+            // labelFIO
+            // 
+            this.labelFIO.AutoSize = true;
+            this.labelFIO.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.labelFIO.Location = new System.Drawing.Point(29, 92);
+            this.labelFIO.Name = "labelFIO";
+            this.labelFIO.Size = new System.Drawing.Size(30, 13);
+            this.labelFIO.TabIndex = 5;
+            this.labelFIO.Text = "ФИО";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.labelPhone.Location = new System.Drawing.Point(29, 174);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(51, 13);
+            this.labelPhone.TabIndex = 6;
+            this.labelPhone.Text = "Телефон";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.labelEmail.Location = new System.Drawing.Point(29, 268);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(105, 13);
+            this.labelEmail.TabIndex = 7;
+            this.labelEmail.Text = "Электронная почта";
+            // 
+            // textBoxBusuness
+            // 
+            this.textBoxBusuness.Location = new System.Drawing.Point(32, 52);
+            this.textBoxBusuness.Name = "textBoxBusuness";
+            this.textBoxBusuness.Size = new System.Drawing.Size(100, 21);
+            this.textBoxBusuness.TabIndex = 8;
+            // 
+            // textBoxFIO
+            // 
+            this.textBoxFIO.Location = new System.Drawing.Point(32, 129);
+            this.textBoxFIO.Name = "textBoxFIO";
+            this.textBoxFIO.Size = new System.Drawing.Size(100, 21);
+            this.textBoxFIO.TabIndex = 9;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(32, 216);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(100, 21);
+            this.textBoxPhone.TabIndex = 10;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(32, 310);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 21);
+            this.textBoxEmail.TabIndex = 11;
+            // 
+            // listViewClients
+            // 
+            this.listViewClients.BackColor = System.Drawing.SystemColors.Window;
+            this.listViewClients.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listViewClients.FullRowSelect = true;
+            this.listViewClients.GridLines = true;
+            this.listViewClients.HideSelection = false;
+            this.listViewClients.Location = new System.Drawing.Point(199, 92);
+            this.listViewClients.MultiSelect = false;
+            this.listViewClients.Name = "listViewClients";
+            this.listViewClients.Size = new System.Drawing.Size(308, 177);
+            this.listViewClients.TabIndex = 12;
+            this.listViewClients.UseCompatibleStateImageBehavior = false;
+            this.listViewClients.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Id";
+            this.columnHeader1.Width = 37;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Организация";
+            this.columnHeader2.Width = 79;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ФИО";
+            this.columnHeader3.Width = 50;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Телефон";
+            this.columnHeader4.Width = 68;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Почта";
+            this.columnHeader5.Width = 68;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::РосБизнесСофт.Properties.Resources._201701151112201369185589;
+            this.pictureBox1.Location = new System.Drawing.Point(316, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormClients
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(539, 350);
+            this.Controls.Add(this.listViewClients);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.textBoxFIO);
+            this.Controls.Add(this.textBoxBusuness);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelFIO);
+            this.Controls.Add(this.labelBusuness);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Roboto Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "FormClients";
+            this.Text = "Клиенты";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.Label labelBusuness;
+        private System.Windows.Forms.Label labelFIO;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxBusuness;
+        private System.Windows.Forms.TextBox textBoxFIO;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.ListView listViewClients;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
--- РосБизнесСофт/Properties/Resources.Designer.cs
+++ РосБизнесСофт/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace РосБизнесСофт.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap orig {
+            get {
+                object obj = ResourceManager.GetObject("orig", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
